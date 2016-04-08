5fe74cc818dc2be05aa387f9fe14eaaa2e160f97
Riley Trautman <asonix.dev@gmail.com>
2016-04-08 15:00:24 -0500

    initial commit

---
+++ installer.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# I need to evaluate symlinks because idk where a userwould call this from
+SOURCE="${BASH_SOURCE}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, it needs to be resolved
+done
+DIR="$( cd "$( dirname "$SOURCE}" )" && pwd )"
+
+# setup neovim
+mkdir -p "$HOME/.config/nvim/autoload"
+cp "$DIR/init.vim" "$HOME/.config/nvim/"
+
+if [ ! -f "$HOME/.config/nvim/autoload/plug.vim" ]; then
+  curl -fLo "$HOME/.config/nvim/autoload/plug.vim" \
+    "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+fi
+
+cp -r "$DIR/i3*" "$HOME/.config"
+cp -r "$DIR/.zsh*" "$HOME"
+cp -R "$DIR/.profile" "$HOME"
+cp "$DIR/subtle.rb" "$HOME/.config"
+cp "$DIR/compton.conf" "$HOME/.config"
+cp "$DIR/.Xresources" "$HOME"
+
+cp -r "$DIR/Scripts" "$HOME"
+
+mkdir -p "$HOME/Development/git"
+
+pushd $(pwd) > /dev/null
+
+if [ -d "$HOME/Development/git/base16-gnome-terminal" ]; then
+  cd "$HOME/Development/git/base16-gnome-terminal"
+  git pull
+else
+  cd "$HOME/Development/git/"
+  git clone "https://github.com/chriskempson/base16-gnome-terminal.git"
+fi
+
+if [ -d "$HOME/Development/git/base16-pantheon-terminal" ]; then
+  cd "$HOME/Development/git/base16-pantheon-terminal"
+  git pull
+else
+  cd "$HOME/Development/git/"
+  git clone "https://github.com/charlesbjohnson/base16-pantheon-terminal.git"
+fi
+
+if [ -d "$HOME/Development/git/base16-shell" ]; then
+  cd "$HOME/Development/git/base16-shell"
+  git pull
+else
+  git clone "https://github.com/chriskempson/base16-shell.git"
+fi
+
+popd > /dev/null
---
+++ updater.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# I need to evaluate symlinks because i'm linking to this script in $HOME/Scripts
+SOURCE="${BASH_SOURCE}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, it needs to be resolved
+done
+DIR="$( cd "$( dirname "$SOURCE}" )" && pwd )"
+
+cp -r "$HOME/.config/i3*" "$DIR" # copy i3 config and i3status config
+cp -r "$HOME/.zsh*" "$DIR" # copy zsh configs
+cp -r "$HOME/.profile" "$DIR" # copy profile
+cp "$HOME/.config/nvim/init.vim" "$DIR" # copy nvim config
+cp "$HOME/.config/subtle.rb" "$DIR" # copy subtle config
+cp "$HOME/.config/compton.conf" "$DIR" # copy compton config
+cp "$HOME/.Xresources" "$DIR" # copy xterm and urxvt configs
+
+cp -r "$HOME/Scripts" "$DIR" # copy scripts
+
+pushd $(pwd) > /dev/null
+
+cd "$DIR"
+
+git add .
+git commit -m "Updated Configs"
+git push
+
+popd > /dev/null
+
