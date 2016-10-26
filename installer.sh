b603fad3d7146160db5ff7b581f98b102d32da4a
Riley Trautman <asonix.dev@gmail.com>
2016-10-25 21:58:10 -0500

    Abstract functionality, set term font

--- installer.sh
+++ installer.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+function git_package() {
+  if [ -d "$HOME/Development/$3/$1" ]; then
+    cd "$HOME/Development/$3/$1"
+    git pull
+  else
+    cd "$HOME/Development/$3/"
+    git clone "https://github.com/$2/$1"
+  fi
+}
+
+function aur_package() {
+  cd /tmp
+  curl -O "https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz"
+  tar zxvf $1.tar.gz
+  cd $1
+  makepkg -si --noconfirm
+  cd /tmp
+  rm -rf $1*
+}
+
 LSB_RELEASE=/etc/lsb-release
 OS_RELEASE=/etc/os-release
 
@@ -53,21 +73,9 @@ then
   cd ~/.gnupg
   sed -i 's/#keyserver-options.*/keyserver hkp:\/\/keys.gnupg.net\nkeyserver-options auto-key-retrieve/g' gpg.conf
 
-  cd /tmp
-  curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/cower.tar.gz
-  tar zxvf cower.tar.gz
-  cd cower
-  makepkg -si --noconfirm
-  cd /tmp
-  rm -rf cower*
+  aur_package 'cower'
+  aur_package 'pacaur'
 
-  cd /tmp
-  curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz
-  tar zxvf pacaur.tar.gz
-  cd pacaur
-  makepkg -si --noconfirm
-  cd /tmp
-  rm -rf pacaur*
   popd > /dev/null
 
   # i3-gaps
@@ -122,68 +130,53 @@ cp "$DIR/.Xresources" "$HOME"
 mkdir -p "$HOME/.local"
 cp -r "$DIR/bin" "$HOME/.local"
 
-if [ -d "$HOME/Development/git/base16-gnome-terminal" ]; then
-  cd "$HOME/Development/git/base16-gnome-terminal"
-  git pull
-else
-  cd "$HOME/Development/git/"
-  git clone "https://github.com/chriskempson/base16-gnome-terminal.git"
-fi
-
-if [ -d "$HOME/Development/git/base16-pantheon-terminal" ]; then
-  cd "$HOME/Development/git/base16-pantheon-terminal"
-  git pull
-else
-  cd "$HOME/Development/git/"
-  git clone "https://github.com/charlesbjohnson/base16-pantheon-terminal.git"
-fi
-
-if [ -d "$HOME/Development/git/base16-shell" ]; then
-  cd "$HOME/Development/git/base16-shell"
-  git pull
-else
-  cd "$HOME/Development/git/"
-  git clone "https://github.com/chriskempson/base16-shell.git"
-fi
+git_package 'base16-gnome-terminal' 'chriskempson' 'git'
+git_package 'base16-pantheon-terminal' 'charlesbjohnson' 'git'
+git_package 'base16-shell' 'chriskempson' 'git'
 
 mkdir -p "$HOME/Development/cpp"
-if [ -d "$HOME/Development/cpp/gnome-background-rotation" ]; then
-  cd "$HOME/Development/cpp/gnome-background-rotation"
-  git pull
-else
-  cd "$HOME/Development/cpp/"
-  git clone "https://github.com/asonix/gnome-background-rotation.git"
-fi
+git_package 'gnome-background-rotation' 'asonix' 'cpp'
 
 if [ "$ID" == "arch" ]; then
   pacaur -S osx-arc-white-git --noconfirm
+  pacaur -S ttf-monofur-powerline-git --noconfirm
+  pacaur -S ttf-fantasque-sans --noconfirm
 else
-  if [ -d "$HOME/Development/git/OSX-Arc-White" ]; then
-    cd "$HOME/Development/git/OSX-Arc-White"
-    git pull
-  else
-    cd "$HOME/Development/git/"
-    git clone "https://github.com/fusion809/OSX-Arc-White.git"
-  fi
+  git_package 'OSX-Arc-White' 'fusion809' 'git'
   sudo cp -r "$HOME/Development/git/OSX-Arc-White" /usr/share/themes/
+
+  # Fonts
+  git_package 'ttf-monofur-powerline' 'rsrsl' 'git'
+  sudo cp "$HOME/Development/git/ttf-monofur-powerline/*.ttf" /usr/share/fonts/TTF
+
+  cd "$HOME/Downloads"
+  curl -O 'https://fontlibrary.org/assets/downloads/fantasque-sans-mono/b0cbb25e73a9f8354e96d89524f613e7/fantasque-sans-mono.zip'
+  unzip fantasque-sans-mono.zip
+  sudo cp "$HOME/Downloads/fantasque-sans-mono/*.ttf" /usr/share/fonts/TTF
+
+  sudo fc-cache
 fi
 
 # Install Elm
 sudo npm install -g elm
 
 # Set Themes
+gsettings set org.gnome.desktop.interface icon-theme 'Paper'
 gsettings set org.gnome.desktop.interface gtk-theme 'OSX-Arc-White'
+
 gsettings set com.solus-project.budgie-helper.workarounds fix-button-layout 'close,minimize,maximize:menu'
 gsettings set com.solus-project.budgie-wm button-layout 'close,minimize,maximize:appmenu'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:appmenu'
 gsettings set org.gnome.shell.extensions.classic-overrides button-layout 'close,minimize,maximize:appmenu'
-gsettings set org.gnome.desktop.interface icon-theme 'Paper'
+
 gsettings set org.gnome.shell enabled-extensions "['user-theme@gnome-shell-extensions.gcampax.github.com']"
 gsettings set org.gnome.shell.extensions.user-theme name 'Arc-Dark'
+
 gsettings set org.pantheon.terminal.settings background 'rgba(45, 45, 45, 0.98)'
 gsettings set org.pantheon.terminal.settings follow-last-tab true
 gsettings set org.pantheon.terminal.settings natural-copy-paste false
 gsettings set org.pantheon.terminal.settings tab-bar-behavior 'Hide When Single Tab'
+gsettings set org.pantheon.terminal.settings font 'fantasque sans mono 12'
 
 # Setup Vim Plug
 nvim -c PlugInstall -c qa
